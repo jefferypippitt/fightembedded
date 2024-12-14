904d259d5d9ef3fc806d1461261f1ce39f5b42a3
Jeffery <jefferyspippitt@gmail.com>
2024-12-14 16:56:57 -0600

    google search

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,9 +29,10 @@ export const metadata: Metadata = {
     siteName: "Fight Embedded",
     images: [
       {
-        url: "/og-image.jpg",
+        url: "https://fightembedded.com/og-image.jpg",
         width: 1200,
         height: 630,
+        alt: "Fight Embedded - UFC Athletes' Stats and Performance Analytics",
       },
     ],
   },
