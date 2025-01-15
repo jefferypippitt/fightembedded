ec989083e8bb7a2ca1eb728ba8c85a8545e14a7a
Jeffery <jefferyspippitt@gmail.com>
2025-01-14 19:21:28 -0600

    fix

--- components/athlete-form.tsx
+++ components/athlete-form.tsx
@@ -248,8 +248,7 @@ export function AthleteForm({ initialData }: AthleteFormProps) {
                     <UploadButton
                       endpoint="athleteImage"
                       appearance={{
-                        button:
-                          "inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border bg-white text-black hover:bg-accent hover:text-accent-foreground h-9 px-3 dark:bg-background dark:text-foreground",
+                        button: "inline-flex items-center justify-center rounded-md text-sm font-medium px-3 h-9 border bg-background text-foreground",
                         allowedContent: "hidden",
                       }}
                       content={{
